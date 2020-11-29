d54a2a796aef2cd06841301bdabc0ad535f85b26
Nishant Gurrapadi <nishant.liteteesko@gmail.com>
2020-11-29 16:12:08 -0700

    Add link to paper in comment

--- swarmintelligence/aco/ant_colony_optimization.py
+++ swarmintelligence/aco/ant_colony_optimization.py
@@ -6,6 +6,9 @@ from .graph import ACOGraph
 
 class AntColonyOptimization:
     def __init__(self, num_ants, evaporation_rate, num_iter):
+        """
+        Link to paper: http://users.dimi.uniud.it/~antonio.dangelo/Robotica/2018/helper/10.1.1.26.1865.pdf
+        """
         self.num_ants = num_ants
         self.num_iter = num_iter
         self.evaporation_rate = evaporation_rate
